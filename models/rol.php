d690d5de0cfcebecd3495324cdc0fb1047c7b18d
melani-x <melanihg73@gmail.com>
2026-05-12 18:11:14 -0600

    Cambios en rol

--- models/rol.php
+++ models/rol.php
@@ -4,6 +4,8 @@ require_once __DIR__ . "/../config/database.php";
 
 class Rol {
 
+
+     //Consulta
     public static function obtenerTodos() {
 
         global $conn;
@@ -20,6 +22,55 @@ class Rol {
         }
 
         return $roles;
+    }
+      // (Buscar) Obtener rol por id
+    public static function obtenerPorId($id) {
+
+        global $conn;
+
+        $sql = "SELECT * FROM roles WHERE id = $id";
+
+        $resultado = $conn->query($sql);
+
+        return $resultado->fetch_assoc();
+    }
+    //Crear
+     public static function crear($data) {
+
+        global $conn;
+
+        $sql = "INSERT INTO roles (
+            nombre
+        ) VALUES (
+            '{$data['nombre']}'
+        )";
+
+        return $conn->query($sql);
+    }
+
+    //Actualizar
+        public static function actualizar($id, $data) {
+
+        global $conn;
+
+        $sql = "UPDATE roles SET
+
+            nombre = '{$data['nombre']}'
+
+            WHERE id = $id
+        ";
+
+        return $conn->query($sql);
+    }
+
+    //Borrar
+     public static function eliminar($id) {
+
+        global $conn;
+
+        $sql = "DELETE FROM roles WHERE id = $id";
+
+        return $conn->query($sql);
     }
 }
 ?>
