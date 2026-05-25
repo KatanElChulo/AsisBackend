992b0fc902d7748dfe927dffc24bbd415ee51e2e
Your Name <imbotgames30@gmail.com>
2026-05-25 16:18:53 -0600

    Prueba

--- api/empleados.php
+++ api/empleados.php
@@ -4,8 +4,8 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 
-require_once "../controllers/empleadosController.php";
-
+require_once __DIR__ . "/../../controllers/empleadosController.php";
+require_once __DIR__ . "/../../config/database.php";
 $controller = new EmpleadosController();
 
 $method = $_SERVER['REQUEST_METHOD'];
