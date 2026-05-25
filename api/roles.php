59be89302fe2f4b9607815cb5c46fb1ca5d7a35e
Your Name <imbotgames30@gmail.com>
2026-05-25 17:34:03 -0600

    asda

--- api/roles.php
+++ api/roles.php
@@ -7,8 +7,13 @@ error_reporting(E_ALL);
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 
+require_once __DIR__ . "/../config/database.php";
+
 echo json_encode([
-    "paso" => "roles.php sí cargó"
+    "paso" => "database.php cargó",
+    "conn_existe" => isset($conn),
+    "conexion_existe" => isset($conexion),
+    "mysqli_existe" => isset($mysqli)
 ]);
 
 exit;
