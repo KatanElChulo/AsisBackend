e22e8c16a061ab541d7fa3ac089f56706464b91e
Your Name <imbotgames30@gmail.com>
2026-05-25 17:33:10 -0600

    ad

--- api/roles.php
+++ api/roles.php
@@ -1,42 +1,14 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
 
-if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
-    exit;
-}
+echo json_encode([
+    "paso" => "roles.php sí cargó"
+]);
 
-require_once __DIR__ . "/../controllers/RolesController.php";
-
-$controller = new RolesController();
-
-$method = $_SERVER["REQUEST_METHOD"];
-$id = $_GET["id"] ?? null;
-
-switch ($method) {
-    case "GET":
-        $controller->index();
-        break;
-
-    case "POST":
-        $controller->store();
-        break;
-
-    case "PUT":
-        $controller->update($id);
-        break;
-
-    case "DELETE":
-        $controller->delete($id);
-        break;
-
-    default:
-        echo json_encode([
-            "success" => false,
-            "message" => "Método no permitido"
-        ]);
-        break;
-}
+exit;
