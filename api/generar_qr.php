ba0dbdb6e5aa1c41cccb47f95f6318aaa0dcf555
Your Name <imbotgames30@gmail.com>
2026-05-25 19:40:27 -0600

    dasd

--- api/generar_qr.php
+++ api/generar_qr.php
@@ -1,30 +1,15 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
 
-require_once "../config/conexion.php";
+echo json_encode([
+    "success" => true,
+    "message" => "generar_qr.php sí cargó"
+]);
 
-$token = bin2hex(random_bytes(32));
-$expira_en = date("Y-m-d H:i:s", strtotime("+30 seconds"));
-
-$sql = "INSERT INTO qr_tokens (token, tipo, expira_en)
-        VALUES (?, 'general', ?)";
-
-$stmt = $conexion->prepare($sql);
-$stmt->bind_param("ss", $token, $expira_en);
-
-if ($stmt->execute()) {
-    echo json_encode([
-        "success" => true,
-        "token" => $token,
-        "expira_en" => $expira_en
-    ]);
-} else {
-    echo json_encode([
-        "success" => false,
-        "message" => "Error al generar QR"
-    ]);
-}
+exit;
