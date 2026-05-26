2c6a4b729d266b3abfd45f4d04f3d61c40df9831
Your Name <imbotgames30@gmail.com>
2026-05-25 19:47:59 -0600

    asd

--- api/generar_qr.php
+++ api/generar_qr.php
@@ -1,15 +1,16 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 
+require_once __DIR__ . "/../config/database.php";
+
 echo json_encode([
     "success" => true,
-    "message" => "generar_qr.php sí cargó"
+    "message" => "database.php cargó",
+    "conn_existe" => isset($conn),
+    "conexion_existe" => isset($conexion),
+    "mysqli_existe" => isset($mysqli)
 ]);
 
 exit;
